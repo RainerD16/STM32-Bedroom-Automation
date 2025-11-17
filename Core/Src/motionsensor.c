a922cce0ff44c37eab775c3b3e77a5c96ae51ddc
RainerD <rainer.ds03@gmail.com>
2025-11-17 12:40:02 -0800

    Initial Commit

---
+++ Core/Src/motionsensor.c
@@ -0,0 +1,27 @@
+/*
+ * motionsensor.c
+ *
+ *  Created on: Nov 17, 2025
+ *      Author: Rainer
+ */
+
+#include "motionsensor.h"
+#include "stm32l4xx_hal.h"
+
+int32_t motion;
+char msg1[] = "Motion Detected (1) \r\n";
+char msg2[] = "No Motion Detected (0) \r\n";
+
+int32_t readMotion(){
+	motion = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_5);
+	return motion;
+}
+
+const char* displayMotion(){
+	if(motion){
+		return msg1;
+	}else{
+		return msg2;
+	}
+}
+
---
+++ Core/Src/motionsensor.h
@@ -0,0 +1,17 @@
+/*
+ * motionsensor.h
+ *
+ *  Created on: Nov 17, 2025
+ *      Author: Rainer
+ */
+
+#ifndef SRC_MOTIONSENSOR_H_
+#define SRC_MOTIONSENSOR_H_
+#include <stdio.h>
+#include <stdlib.h>
+
+int32_t readMotion(void);
+const char* displayMotion(void);
+
+
+#endif /* SRC_MOTIONSENSOR_H_ */
