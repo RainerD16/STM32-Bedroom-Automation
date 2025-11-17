2f2215c743f346a9f028d9010a3dd941f1a959fd
RainerD <rainer.ds03@gmail.com>
2025-11-17 14:25:04 -0800

    Edited motionsensor.h

--- Core/Inc/motionsensor.h
+++ Core/Inc/motionsensor.h
@@ -8,16 +8,10 @@
 #ifndef INC_MOTIONSENSOR_H_
 #define INC_MOTIONSENSOR_H_
 
-#ifndef SRC_MOTIONSENSOR_H_
-#define SRC_MOTIONSENSOR_H_
 #include <stdio.h>
 #include <stdlib.h>
 
 int32_t readMotion(void);
 const char* displayMotion(void);
-
-
-#endif /* SRC_MOTIONSENSOR_H_ */
-
 
 #endif /* INC_MOTIONSENSOR_H_ */
